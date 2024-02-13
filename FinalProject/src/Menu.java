22e6e2fea5d928872560eb6d83f58fd8f6446687
Maria Paula <m.gonzaleze@uniandes.edu.co>
2024-02-13 15:43:21 -0500

    Add: Initialize 2 teachers of each type

--- FinalProject/src/Menu.java
+++ FinalProject/src/Menu.java
@@ -2,6 +2,26 @@ import java.util.Scanner;
 
 public class Menu {
     public static void VirtualUniversity() {
+
+        //Initialize 2 teachers of each type
+        FullTimeTeacher fullTimeTeacher1 = new FullTimeTeacher("Carlos Acosta", 35);
+        double salaryFullTimeTeacher1 = fullTimeTeacher1.calculateSalary();
+        fullTimeTeacher1.setSalary(salaryFullTimeTeacher1);
+
+        FullTimeTeacher fullTimeTeacher2 = new FullTimeTeacher("Marta Bolaños", 40);
+        double salaryFullTimeTeacher2 = fullTimeTeacher2.calculateSalary();
+        fullTimeTeacher2.setSalary(salaryFullTimeTeacher2);
+
+        PartTimeTeacher partTimeTeacher1 = new PartTimeTeacher("Angie Castañeda",3);
+        double salaryPartTimeTeacher1 = partTimeTeacher1.calculateSalary();
+        partTimeTeacher1.setSalary(salaryPartTimeTeacher1);
+
+        PartTimeTeacher partTimeTeacher2 = new PartTimeTeacher("Diego Diaz",1);
+        double salaryPartTimeTeacher2 = partTimeTeacher2.calculateSalary();
+        partTimeTeacher2.setSalary(salaryPartTimeTeacher2);
+
+
+        //************************************************************
         University university = new University();
         Scanner scan = new Scanner(System.in);
 
