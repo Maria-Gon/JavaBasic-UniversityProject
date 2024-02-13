f8a913457541dc540252fcc3e05ac8aea1268db9
Maria Paula <m.gonzaleze@uniandes.edu.co>
2024-02-13 15:30:25 -0500

    Add: Years of experience attribute

--- FinalProject/src/FullTimeTeacher.java
+++ FinalProject/src/FullTimeTeacher.java
@@ -1,5 +1,6 @@
 public class FullTimeTeacher extends Teacher implements Salary {
     private static final double multiplier = 1.1;
+    private int yearsOfExperience;
 
     public FullTimeTeacher(String name) {
         super(name);
