a54f294d93b7c6ed12a187a91781b26873f62757
Maria Paula <m.gonzaleze@uniandes.edu.co>
2024-02-13 14:20:53 -0500

    Add: Getters

--- FinalProject/src/Teacher.java
+++ FinalProject/src/Teacher.java
@@ -1,4 +1,13 @@
 public class Teacher {
     protected String name;
     protected double salary;
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
 }
