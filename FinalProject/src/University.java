ee47431e42978e1152ce07c905b15efe886d07aa
Maria Paula <m.gonzaleze@uniandes.edu.co>
2024-02-13 15:55:12 -0500

    Add: Add teacher to university

--- FinalProject/src/University.java
+++ FinalProject/src/University.java
@@ -4,4 +4,8 @@ public class University {
     private List<Student> students;
     private List<Teacher> teachers;
     private List<Class> classes;
+
+    public void addTeacher(Teacher teacher){
+        teachers.add(teacher);
+    }
 }
