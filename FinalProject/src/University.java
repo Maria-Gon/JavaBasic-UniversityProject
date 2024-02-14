490439c5b1ea1f206f30b77b922c9b4366b80e2c
Maria Paula <m.gonzaleze@uniandes.edu.co>
2024-02-13 23:42:20 -0500

    Add: University constructor

--- FinalProject/src/University.java
+++ FinalProject/src/University.java
@@ -1,9 +1,16 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class University {
     private List<Student> students;
     private List<Teacher> teachers;
     private List<Class> classes;
+
+    public University() {
+        this.students = new ArrayList<>();
+        this.teachers = new ArrayList<>();
+        this.classes = new ArrayList<>();
+    }
 
     public void addTeacher(Teacher teacher){
         teachers.add(teacher);
